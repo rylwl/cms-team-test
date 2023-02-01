c5ef8d5cb978b2c8cc681651317ee2da70391574
Rylwl <928736874@qq.com>
2023-02-01 14:39:27 +0800

    first commit

---
+++ src/api/paper-type/controllers/paper-type.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * paper-type controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::paper-type.paper-type');
---
+++ src/api/paper-type/routes/paper-type.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * paper-type router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::paper-type.paper-type');
